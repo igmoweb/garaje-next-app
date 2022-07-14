ec965a5c435abc17c70c141b8192255c9152088e
Ignacio Cruz <igmoweb@gmail.com>
2022-07-14 13:31:35 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, env) => {
+    const { dev } = env;
+    if (dev) {
+      config.plugins.push(
+        new ESLintPlugin({
+          extensions: ['js', 'tsx', 'ts', 'jsx'],
+          files: ['pages', 'src'],
+          failOnError: false,
+        }),
+      );
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
